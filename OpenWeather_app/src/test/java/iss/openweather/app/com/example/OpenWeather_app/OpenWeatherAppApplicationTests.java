ebb85aea8cc4f2fb39c01919c6ab430c81848d6a
Elvinardy Darwin <elvinardy_darwin@Elvinardys-MacBook-Pro.local>
2022-02-08 13:08:28 +0800

    day7 1st commit

---
+++ OpenWeather_app/src/test/java/iss/openweather/app/com/example/OpenWeather_app/OpenWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package iss.openweather.app.com.example.OpenWeather_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
